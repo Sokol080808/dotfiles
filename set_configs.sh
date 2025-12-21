bb0c4006e86089d66762295b5475e60ee35da095
Alex Sokolnikov <assokolnikov@gmail.com>
2025-12-21 23:54:26 +0300

    Updated install script

--- set_configs.sh
+++ set_configs.sh
@@ -4,5 +4,17 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # .config
+config_symlink() {
+  if [[ -e "$HOME/.config/$1" ]]; then
+    if [[ -L "$HOME/.config/$1" ]]; then
+      rm "$HOME/.config/$1"
+    else
+      rm -rf "$HOME/.config/$1"
+    fi
+  fi
+
+  ln -s "$SCRIPT_DIR/$1" "$HOME/.config/$1"
+}
+
 mkdir -p ~/.config
-ln -sf "$SCRIPT_DIR/nvim" ~/.config/nvim
+config_symlink "nvim"
