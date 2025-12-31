9182ab80a6d8d9ad7d86551fc6bfa9b0ffc173c6
Aleksey Sokolnikov <assokolnikov@gmail.com>
2026-01-01 02:21:19 +0600

    Updated setting

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.termguicolors = true
+
 vim.opt.showmode = false
 
 vim.opt.shiftwidth = 4
@@ -14,8 +16,17 @@ vim.opt.autoindent = true
 
 vim.opt.number = true
 
-vim.opt.termguicolors = true
+vim.opt.list = true
+vim.opt.listchars = {
+    eol = '↵',
+    tab = '→ ',
+    extends = '⟩',
+    precedes = '⟨',
+    trail = '·',
+}
 
+vim.opt.signcolumn = "yes"
 vim.diagnostic.config({
-    virtual_text = { current_line = true }
+    virtual_text = { current_line = true },
+    severity_sort = true,
 })
