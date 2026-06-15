963fe11420c58562386003e5cc30d83f7a7b8bab
Aleksey Sokolnikov <assokolnikov@gmail.com>
2026-06-15 15:09:49 +0300

    changed theme to tokyonight:night

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,7 +2,7 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = { style = "night" },
     config = function(_, opts)
         require("tokyonight").setup(opts)
         vim.cmd [[colorscheme tokyonight]]
