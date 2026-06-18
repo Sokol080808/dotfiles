611d90ab117aedb876280ff532f734efd3d8cb68
Aleksey Sokolnikov <assokolnikov@gmail.com>
2026-06-18 23:22:39 +0300

    changed theme to tokyonight:storm

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -2,7 +2,7 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { style = "night" },
+    opts = { style = "storm" },
     config = function(_, opts)
         require("tokyonight").setup(opts)
         vim.cmd [[colorscheme tokyonight]]
